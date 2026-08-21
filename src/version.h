cca6da214c7230be02ac99da7b6545de70f4c0ac
Korbinian Krügl <korbi0312@users.noreply.github.com>
2026-08-21 18:08:14 +0200

    Remove VNC connect hint, tag v1.1.8

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 #pragma once
-const char GIT_COMMIT_HASH[] = "e71c118c53590fce4a6f54361ff77c3a79a50843";
+const char GIT_COMMIT_HASH[] = "47695959899fd9eb662be5fa1fdce3b9c892ef93";
 const char GIT_TAG[] = "v1.1.7";
